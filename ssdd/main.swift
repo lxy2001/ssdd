7f798e6c55e2c194340c57906ebb24e203899551
20181106279 <1771478860@qq.com>
2019-09-10 17:42:48 +0800

    Initial Commit

---
+++ ssdd/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ssdd
+//
+//  Created by 20181106279 on 2019/9/10.
+//  Copyright © 2019 20181106279. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
